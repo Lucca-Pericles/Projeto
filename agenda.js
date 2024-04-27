207c6d4804c81e51168fb683a3dbbb830e6d6175
Lucca-Pericles <163687798+Lucca-Pericles@users.noreply.github.com>
2024-04-26 22:10:28 -0300

    tentativa

---
+++ agenda.js
@@ -0,0 +1,7 @@
+const cabecalho = document.querySelector( "#cabecalho ");
+const btn_home = document.querySelector( "#home");
+const btn_nome = document.querySelector( "#nome");
+const  btn_pesquisar = document.querySelector('#pesquisar');
+const  btn_gestao = document.querySelector('#gestao');
+const  btn_sobre = document.querySelector('#sobre');
+const  principal= document.querySelector('#principal');
